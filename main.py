f0449218999f79319193a720ef802a1bd94a0592
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-06-22 19:50:59 +0200

    feat: finding and selecting particular elements with beautifulsoup

--- main.py
+++ main.py
@@ -5,4 +5,18 @@ with open("./website.html") as file:
 
 
 soup = BeautifulSoup(contents, "html.parser")
-# print(soup.title.string)
+
+all_anchor_tags = soup.find_all(name="a")
+
+for tag in all_anchor_tags:
+    print(tag.getText())
+    print(tag.get("href"))
+
+heading = soup.find(name="h1", id="name")
+print(heading)
+
+section_heading = soup.find(name="h3", class_="heading")
+print(section_heading.getText())
+
+company_url = soup.select_one(selector="p a")
+print(company_url)
