76bc8e1014ada5faedbf58f01d9c06b57c52e920
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-06-22 20:23:40 +0200

    feat: scraping a live website

--- main.py
+++ main.py
@@ -1,22 +1,46 @@
 from bs4 import BeautifulSoup
+import requests
 
-with open("./website.html") as file:
-    contents = file.read()
+response = requests.get("https://news.ycombinator.com")
+yc_web_page = response.text
+
+soup = BeautifulSoup(yc_web_page, "html.parser")
+articles = soup.find_all(name="a", class_="storylink")
+article_texts = []
+article_links = []
+
+for article_tag in articles:
+    text = article_tag.getText()
+    article_texts.append(text)
+    link = article_tag.get("href")
+    article_links.append(link)
+
+article_upvotes = [int(score.getText().split()[0]) for score in soup.find_all(name="span", class_="score")]
+
+largest_number = max(article_upvotes)
+largest_index = article_upvotes.index(largest_number)
+
+print(article_texts[largest_index])
+print(article_links[largest_index])
 
 
-soup = BeautifulSoup(contents, "html.parser")
+# with open("./website.html") as file:
+#     contents = file.read()
 
-all_anchor_tags = soup.find_all(name="a")
 
-for tag in all_anchor_tags:
-    print(tag.getText())
-    print(tag.get("href"))
+# soup = BeautifulSoup(contents, "html.parser")
 
-heading = soup.find(name="h1", id="name")
-print(heading)
+# all_anchor_tags = soup.find_all(name="a")
 
-section_heading = soup.find(name="h3", class_="heading")
-print(section_heading.getText())
+# for tag in all_anchor_tags:
+#     print(tag.getText())
+#     print(tag.get("href"))
 
-company_url = soup.select_one(selector="p a")
-print(company_url)
+# heading = soup.find(name="h1", id="name")
+# print(heading)
+
+# section_heading = soup.find(name="h3", class_="heading")
+# print(section_heading.getText())
+
+# company_url = soup.select_one(selector="p a")
+# print(company_url)
